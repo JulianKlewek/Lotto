d143fa933346a3ab323af1c6ec6db1f735dd2da2
Julian Klewek <julianklewek@gmail.com>
2024-06-21 14:45:14 +0200

    Add WireMock

--- lotto-engine/src/integration/java/pl.lotto/BaseIntegrationTest.java
+++ lotto-engine/src/integration/java/pl.lotto/BaseIntegrationTest.java
@@ -1,43 +1,58 @@
-//package pl.lotto;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.DynamicPropertyRegistry;
-//import org.springframework.test.context.DynamicPropertySource;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.testcontainers.containers.MongoDBContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import org.testcontainers.utility.DockerImageName;
-//import pl.lotto.numbersgenerator.NumbersGeneratorFacade;
-//
-//import java.util.regex.Pattern;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest(classes = {LottoEngine.class, IntegrationConfiguration.class})
-//@Testcontainers
-//@ActiveProfiles("integration")
-//public class BaseIntegrationTest {
-//
-//    @Autowired
-//    protected MockMvc mockMvc;
-//    @Autowired
-//    protected NumbersGeneratorFacade numbersGeneratorFacade;
-//    @Autowired
-//    protected ResultCheckerFacade resultCheckerFacade;
-//    @Autowired
-//    protected ObjectMapper objectMapper;
-//    @Container
-//    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
-//
-//    @DynamicPropertySource
-//    private static void propertyOverride(DynamicPropertyRegistry registry) {
-//        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-//    }
-//
-//    protected static final Pattern UUID_REGEX =
-//            Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
-//}
+package pl.lotto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import pl.lotto.resultchecker.ResultCheckerFacade;
+import pl.lotto.resultchecker.WinningNumbersPort;
+
+import java.util.regex.Pattern;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = {LottoEngine.class, IntegrationConfiguration.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("integration")
+public class BaseIntegrationTest {
+
+    protected static final String WIREMOCK_SERVER_HOST = "http://localhost";
+
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected WinningNumbersPort winningNumbersPort;
+    @Autowired
+    protected ResultCheckerFacade resultCheckerFacade;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Container
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    @DynamicPropertySource
+    private static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("numbersgenerator.service.url", () -> WIREMOCK_SERVER_HOST + ":" + wireMockServer.getPort());
+        registry.add("spring.cloud.loadbalancer.enabled", () -> true);
+    }
+
+    @RegisterExtension
+    protected static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    protected static final Pattern UUID_REGEX =
+            Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+}
---
+++ numbers-generator/src/integration/java/pl.lotto/feature/GenerateAndSendWinningNumbersIT.java
@@ -0,0 +1,12 @@
+package pl.lotto.feature;
+
+import org.junit.Test;
+import pl.lotto.BaseIntegrationTest;
+
+public class GenerateAndSendWinningNumbersIT extends BaseIntegrationTest {
+
+    @Test
+    public void shouldGenerateAndSendWinningNumbers(){
+
+    }
+}
