a31c6b69e208ea928dfb8d051fd15dc9ea826054
Julian Klewek <julianklewek@gmail.com>
2024-06-27 10:04:59 +0200

    Add propertyConfig for drawDateGenerator

--- lotto-engine/src/test/java/pl/lotto/drawdategenerator/DrawDateGeneratorFacadeConfig.java
+++ lotto-engine/src/test/java/pl/lotto/drawdategenerator/DrawDateGeneratorFacadeConfig.java
@@ -2,4 +2,6 @@ package pl.lotto.drawdategenerator;
 
 class DrawDateGeneratorFacadeConfig implements DrawDateTestsConstants {
 
+    DrawDatePropertyConfigurable propertyConfigurable = new DrawDatePropertyTestConfig(5, 20, 0, 0);
+
 }
